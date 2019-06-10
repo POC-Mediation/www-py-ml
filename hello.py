e26672f64b0e9468a126bf1f02d97b40bd2d6d8a
xavier <xavier.mary@outlook.com>
2019-06-10 11:07:56 +0200

    update myIP - 2019-06-10 11:07:56

--- hello.py
+++ hello.py
@@ -1,42 +1,50 @@
 from cloudant import Cloudant
-import sqlite3
 from flask import Flask, render_template, request, jsonify
 import atexit
 import os
 import json
 import inspect
+#import sqlite3
 
 app = Flask(__name__, static_url_path='')
 
 db_name = 'mydb'
 client = None
 db = None
-db3 = sqlite3.connect('example.db')
+#db3 = sqlite3.connect('example.db')
 
-if 'VCAP_SERVICES' in os.environ:
-    vcap = json.loads(os.getenv('VCAP_SERVICES'))
-    print('Found VCAP_SERVICES')
-    if 'cloudantNoSQLDB' in vcap:
-        creds = vcap['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
+def init() :
+    global creds, user, password, url, client, db
+    if 'VCAP_SERVICES' in os.environ:
+        vcap = json.loads(os.getenv('VCAP_SERVICES'))
+        print('Found VCAP_SERVICES')
+        if 'cloudantNoSQLDB' in vcap:
+            creds = vcap['cloudantNoSQLDB'][0]['credentials']
+            user = creds['username']
+            password = creds['password']
+            url = 'https://' + creds['host']
+            client = Cloudant(user, password, url=url, connect=True)
+            db = client.create_database(db_name, throw_on_exists=False)
+    elif "CLOUDANT_URL" in os.environ:
+        client = Cloudant(os.environ['CLOUDANT_USERNAME'], os.environ['CLOUDANT_PASSWORD'], url=os.environ['CLOUDANT_URL'], connect=True)
         db = client.create_database(db_name, throw_on_exists=False)
-elif "CLOUDANT_URL" in os.environ:
-    client = Cloudant(os.environ['CLOUDANT_USERNAME'], os.environ['CLOUDANT_PASSWORD'], url=os.environ['CLOUDANT_URL'], connect=True)
-    db = client.create_database(db_name, throw_on_exists=False)
-elif os.path.isfile('vcap-local.json'):
-    with open('vcap-local.json') as f:
-        vcap = json.load(f)
-        print('Found local VCAP_SERVICES')
-        creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
-        user = creds['username']
-        password = creds['password']
-        url = 'https://' + creds['host']
-        client = Cloudant(user, password, url=url, connect=True)
-        db = client.create_database(db_name, throw_on_exists=False)
-        db3_cursor = db3.cursor()
+    elif os.path.isfile('vcap-local.json'):
+        with open('vcap-local.json') as f:
+            vcap = json.load(f)
+            print('Found local VCAP_SERVICES')
+            creds = vcap['services']['cloudantNoSQLDB'][0]['credentials']
+            user = creds['username']
+            password = creds['password']
+            url = 'https://' + creds['host']
+            client = Cloudant(user, password, url=url, connect=True)
+            db = client.create_database(db_name, throw_on_exists=False)
+            #db3_cursor = db3.cursor()
+
+init()
+
+def reinit() :
+    client.disconnect()
+    init()
 
 # On IBM Cloud Cloud Foundry, get the port number from the environment variable PORT
 # When running this app on the local machine, default the port to 8000
@@ -51,6 +59,7 @@ def root():
 # * {
 # *     "name": "Bob"
 # * }
+# * https://4569d683-23ea-4885-9890-23e824527523-bluemix.cloudant.com/mydb/_all_docs
 # */
 @app.route('/api/visitors', methods=['GET'])
 def get_visitor():
@@ -81,6 +90,7 @@ def get_visitor():
 #  */
 @app.route('/api/visitors', methods=['POST'])
 def put_visitor():
+    reinit()
     user = request.json['name']
     data = {'name':user}
     if client:
